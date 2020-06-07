bf467790873ae5382b6d060dc76d1b5d05641adf
xianhaoni <xianhaoni@gmail.com>
2020-06-07 08:45:49 +0800

    this time bob works

--- bob/src/main/java/com/thoughtworks/capability/gtb/bob/controller/BobController.java
+++ bob/src/main/java/com/thoughtworks/capability/gtb/bob/controller/BobController.java
@@ -3,15 +3,12 @@ package com.thoughtworks.capability.gtb.bob.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * @author itutry
- * @create 2020-05-25_16:52
- */
+
 @RestController
 public class BobController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello Docker!";
+        return "Hello from Bob!";
     }
 }
