c6b1aebaaf768bf20813843e838f06c1fed3c37c
xianhaoni <xianhaoni@gmail.com>
2020-06-07 14:46:38 +0800

    first-time all works

--- alice/src/main/java/com/thoughtworks/capability/gtb/alice/controller/AliceController.java
+++ alice/src/main/java/com/thoughtworks/capability/gtb/alice/controller/AliceController.java
@@ -1,14 +1,25 @@
 package com.thoughtworks.capability.gtb.alice.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 
 @RestController
 public class AliceController {
 
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello from alice!";
+    public String hello() throws URISyntaxException {
+        RestTemplate restTemplate = new RestTemplate();
+
+//        URI uri = new URI("http://localhost:8081/hello/") ;
+        ResponseEntity<String> res = restTemplate.getForEntity("http://container2:8081/hello/",String.class);
+        return res.getBody();
+
+
     }
 }
